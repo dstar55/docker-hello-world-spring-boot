0b56183260a72315e34c3f8327f3b5b76fa72783
U-DESKTOP-QJSC0PK\PC <dstar55@yahoo.com>
2017-12-18 20:10:26 +0100

    junit tests initial commit

---
+++ src/test/java/com/dockerforjavadevelopers/hello/DummyTest.java
@@ -0,0 +1,13 @@
+package com.dockerforjavadevelopers.hello;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class DummyTest {
+
+  @Test
+  public void aTest() {
+    assertEquals(true, true);
+  }
+}
